75e1aeed9602ed19c53ca6ff2acf1015b0b1f3ab
kiv1n <aloha@kiv1n.ru>
2023-01-30 14:32:14 +0300

    Update versions of some dependecies

--- utils/network.ts
+++ utils/network.ts
@@ -1,18 +1,17 @@
-import { chain, Chain } from "wagmi";
+import { Chain, polygonMumbai, polygon } from "wagmi/chains";
 
 /**
  * Get contracts chain defined in environment.
  */
 export function getContractsChain(): Chain {
   if (
-    process.env.NEXT_PUBLIC_CONTRACTS_CHAIN_ID ===
-    chain.polygonMumbai.id.toString()
+    process.env.NEXT_PUBLIC_CONTRACTS_CHAIN_ID === polygonMumbai.id.toString()
   ) {
-    return chain.polygonMumbai;
+    return polygonMumbai;
   } else {
     console.error(
       "Failed to define the contracts chain, the application uses the Polygon Mainnet"
     );
-    return chain.polygon;
+    return polygon;
   }
 }
