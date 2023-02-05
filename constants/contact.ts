8af1eb512b410cbf41de75947a78f821fc2c838b
kiv1n <aloha@kiv1n.ru>
2023-02-05 23:22:14 +0300

    Replace twitter link

--- constants/contact.ts
+++ constants/contact.ts
@@ -1,5 +1,5 @@
 export const contact = {
   email: "web3goals@gmail.com",
   github: "https://github.com/web3goals",
-  twitter: "https://twitter.com/web3goals",
+  twitter: "https://twitter.com/Web3GoalsSpace",
 };
