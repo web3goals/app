aee670a2d32351d0494f0558c16bfa51d4c2a37a
kiv1n <aloha@kiv1n.ru>
2023-02-09 12:23:23 +0300

    Add under development banner to footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,18 @@
-import { Box, Fab, SxProps, Typography } from "@mui/material";
+import {
+  Alert,
+  AlertTitle,
+  Box,
+  Fab,
+  Snackbar,
+  SxProps,
+  Typography,
+  Link as MuiLink,
+} from "@mui/material";
 import { Container } from "@mui/system";
 import { ThickDivider } from "components/styled";
+import { CONTACTS } from "constants/contacts";
 import Link from "next/link";
+import { useState } from "react";
 
 /**
  * Component with a footer.
@@ -17,6 +28,7 @@ export default function Footer(props: { sx?: SxProps }) {
     >
       <Copyright sx={{ mt: 0, mb: 4 }} />
       <FeedbackPostFab />
+      <UnderDevelopmentBanner />
     </Box>
   );
 }
@@ -48,5 +60,30 @@ function FeedbackPostFab() {
         Post Feedback
       </Fab>
     </Link>
+  );
+}
+
+function UnderDevelopmentBanner() {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <Snackbar
+      open={open}
+      sx={{ bottom: { xs: 90 } }}
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+    >
+      <Alert
+        severity="warning"
+        onClose={() => setOpen(false)}
+        sx={{ width: { xs: 1, md: 1 / 2 } }}
+      >
+        <AlertTitle>The app is under development</AlertTitle>
+        Follow us on{" "}
+        <MuiLink href={CONTACTS.twitter} target="_blank">
+          Twitter
+        </MuiLink>{" "}
+        to find out about the launch of public beta testing and other updates
+      </Alert>
+    </Snackbar>
   );
 }
