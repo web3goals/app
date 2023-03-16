317bc6a6c7c6a54284f9a7d9514ccaebe17c46bf
kiv1n <aloha@kiv1n.ru>
2023-03-16 17:02:57 +0300

    Delete unused components with motivators and proofs

--- pages/goals/[slug].tsx
+++ pages/goals/[slug].tsx
@@ -1,7 +1,4 @@
-import GoalActions from "components/goal/GoalActions";
-import GoalMotivators from "components/goal/GoalMotivators";
 import GoalParams from "components/goal/GoalParams";
-import GoalProofs from "components/goal/GoalProofs";
 import GoalSteps from "components/goal/GoalSteps";
 import Layout from "components/layout";
 import {
@@ -25,7 +22,6 @@ export default function Goal() {
   const { chain } = useNetwork();
   const [goalId, setGoalId] = useState<string | undefined>();
 
-  // State of contract reading to get goal params
   const {
     data: goalParams,
     refetch: refetchGoalParams,
@@ -38,29 +34,11 @@ export default function Goal() {
     enabled: goalId !== undefined,
   });
 
-  // State of contract reading to get goal motivators
-  const {
-    data: goalMotivators,
-    refetch: refetchGoalMotivators,
-    isFetching: isGoalMotivatorsFetching,
-  } = useContractRead({
-    address: getGoalContractAddress(chain),
-    abi: goalContractAbi,
-    functionName: "getMotivators",
-    args: goalId ? [BigNumber.from(goalId)] : undefined,
-    enabled: goalId !== undefined,
-  });
-
   useEffect(() => {
     setGoalId(slug ? (slug as string) : undefined);
   }, [slug]);
 
-  const isDataReady =
-    goalId &&
-    goalParams &&
-    goalMotivators &&
-    !isGoalParamsFetching &&
-    !isGoalMotivatorsFetching;
+  const isDataReady = goalId && goalParams && !isGoalParamsFetching;
 
   return (
     <Layout maxWidth="sm">
@@ -78,30 +56,6 @@ export default function Goal() {
               isAchieved={goalParams.isAchieved}
             />
             <ThickDivider sx={{ mt: 8 }} />
-            <GoalProofs
-              id={goalId}
-              authorAddress={goalParams.authorAddress}
-              isClosed={goalParams.isClosed}
-              verificationRequirement={goalParams.verificationRequirement}
-              onUpdate={() => {
-                refetchGoalParams();
-                refetchGoalMotivators();
-              }}
-              sx={{ mt: 8 }}
-            />
-            <ThickDivider sx={{ mt: 8 }} />
-            <GoalMotivators
-              id={goalId}
-              authorAddress={goalParams.authorAddress}
-              isClosed={goalParams.isClosed}
-              motivators={goalMotivators}
-              onUpdate={() => {
-                refetchGoalParams();
-                refetchGoalMotivators();
-              }}
-              sx={{ mt: 8 }}
-            />
-            <ThickDivider sx={{ mt: 8 }} />
             <GoalSteps
               id={goalId}
               authorAddress={goalParams.authorAddress}
@@ -110,7 +64,6 @@ export default function Goal() {
               verificationRequirement={goalParams.verificationRequirement}
               onUpdate={() => {
                 refetchGoalParams();
-                refetchGoalMotivators();
               }}
               sx={{ mt: 8 }}
             />
