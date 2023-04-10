1faf3e642df7cc493ed79adefd10fccb200a88b5
kiv1n <aloha@kiv1n.ru>
2023-04-10 12:02:03 +0300

    Refactor navigation components

--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -10,6 +10,8 @@ import {
   MenuItem,
   Toolbar,
   Typography,
+  SxProps,
+  Link as MuiLink,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
@@ -25,8 +27,118 @@ import { useAccount } from "wagmi";
  * Component with navigation.
  */
 export default function Navigation() {
+  return (
+    <AppBar
+      color="inherit"
+      position="fixed"
+      sx={{
+        zIndex: (theme) => theme.zIndex.drawer + 1,
+      }}
+    >
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <LogoDesktop sx={{ flexGrow: 1 }} />
+          <LogoMobile sx={{ flexGrow: 1 }} />
+          <NavigationDesktop />
+          <NavigationMobile />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+function LogoDesktop(props: { sx?: SxProps }) {
+  return (
+    <Box
+      sx={{
+        display: { xs: "none", md: "flex" },
+        ...props.sx,
+      }}
+    >
+      <Link href="/" passHref legacyBehavior>
+        <Box component="a" display="flex">
+          <Logo width="153" height="25" />
+        </Box>
+      </Link>
+      <Typography color="text.secondary" variant="caption" ml={1}>
+        {packageJson.version}-{isDev() ? "dev" : "beta"}
+      </Typography>
+    </Box>
+  );
+}
+
+function LogoMobile(props: { sx?: SxProps }) {
+  return (
+    <Box
+      sx={{
+        display: { xs: "flex", md: "none" },
+        flexDirection: "column",
+        ...props.sx,
+      }}
+    >
+      <Link href="/" passHref legacyBehavior>
+        <Box component="a" display="flex">
+          <Logo width="122" height="20" />
+        </Box>
+      </Link>
+      <Typography color="text.secondary" variant="caption">
+        {packageJson.version}-{isDev() ? "dev" : "beta"}
+      </Typography>
+    </Box>
+  );
+}
+
+function NavigationDesktop(props: { sx?: SxProps }) {
   const { isConnected, address } = useAccount();
 
+  return (
+    <Box
+      sx={{
+        display: { xs: "none", md: "flex" },
+        alignItems: "center",
+        ...props.sx,
+      }}
+    >
+      <Link href="/goals/set" legacyBehavior>
+        <Button variant="contained">Set Goal</Button>
+      </Link>
+      {isConnected && (
+        <Link href={`/accounts/${address}`} passHref legacyBehavior>
+          <MuiLink fontWeight={700} color="inherit" ml={3.5}>
+            Account
+          </MuiLink>
+        </Link>
+      )}
+      <Link href="/goals" passHref legacyBehavior>
+        <MuiLink fontWeight={700} color="inherit" ml={3.5}>
+          Explore
+        </MuiLink>
+      </Link>
+      <Box ml={3.5}>
+        <ConnectButton showBalance={false} accountStatus="full" />
+      </Box>
+      <IconButton
+        component="a"
+        target="_blank"
+        href={CONTACTS.github}
+        sx={{ color: "black", ml: 1.5 }}
+      >
+        <GitHub />
+      </IconButton>
+      <IconButton
+        component="a"
+        target="_blank"
+        href={CONTACTS.twitter}
+        sx={{ color: "black" }}
+      >
+        <Twitter />
+      </IconButton>
+    </Box>
+  );
+}
+
+function NavigationMobile(props: { sx?: SxProps }) {
+  const { isConnected, address } = useAccount();
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
 
@@ -38,194 +150,86 @@ export default function Navigation() {
   }
 
   return (
-    <AppBar
-      color="inherit"
-      position="fixed"
+    <Box
       sx={{
-        zIndex: (theme) => theme.zIndex.drawer + 1,
+        display: { xs: "flex", md: "none" },
+        alignItems: "center",
+        ...props.sx,
       }}
     >
-      <Container maxWidth="lg">
-        <Toolbar disableGutters>
-          {/* Logo */}
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: "flex",
-              flexDirection: { xs: "column", md: "row" },
-            }}
-          >
-            <Link href="/" passHref legacyBehavior>
-              <a style={{ display: "flex" }}>
-                <Logo width="153" height="25" />
-              </a>
-            </Link>
-            <Typography
-              color="text.secondary"
-              variant="body2"
-              sx={{ ml: { md: 1 } }}
-            >
-              {packageJson.version}-{isDev() ? "dev" : "beta"}
-            </Typography>
-          </Box>
-          {/* Set goal button */}
-          <Box
-            sx={{ display: { xs: "none", md: "block" }, flexGrow: 0, mr: 3.5 }}
-          >
-            <Link href="/goals/set" legacyBehavior>
-              <Button variant="contained">Set Goal</Button>
-            </Link>
-          </Box>
-          {/* Account link */}
-          {isConnected && (
-            <Box
-              sx={{
-                display: { xs: "none", md: "block" },
-                flexGrow: 0,
-                mr: 3.5,
-              }}
-            >
-              <Link href={`/accounts/${address}`} passHref legacyBehavior>
-                <Typography
-                  component="a"
-                  sx={{
-                    fontWeight: 700,
-                    color: "inherit",
-                    textDecoration: "none",
-                  }}
-                >
-                  Account
-                </Typography>
-              </Link>
-            </Box>
-          )}
-          {/* Explore goals link */}
-          <Box
-            sx={{ display: { xs: "none", md: "block" }, flexGrow: 0, mr: 3.5 }}
-          >
-            <Link href="/goals" passHref legacyBehavior>
-              <Typography
-                component="a"
-                sx={{
-                  fontWeight: 700,
-                  color: "inherit",
-                  textDecoration: "none",
-                }}
-              >
-                Explore
-              </Typography>
-            </Link>
-          </Box>
-          {/* Connect button */}
-          <Box sx={{ flexGrow: 0, mr: { xs: 0, md: 2 } }}>
-            <ConnectButton
-              showBalance={false}
-              accountStatus={{ smallScreen: "address", largeScreen: "full" }}
-            />
-          </Box>
-          {/* GitHub link */}
-          <Box sx={{ display: { xs: "none", md: "block" }, flexGrow: 0 }}>
-            <IconButton
-              href={CONTACTS.github}
-              target="_blank"
-              component="a"
-              sx={{ color: "#000000" }}
-            >
-              <GitHub />
-            </IconButton>
-          </Box>
-          {/* Twitter link */}
-          <Box sx={{ display: { xs: "none", md: "block" }, flexGrow: 0 }}>
-            <IconButton
-              href={CONTACTS.twitter}
-              target="_blank"
-              component="a"
-              sx={{ color: "#000000" }}
-            >
-              <Twitter />
-            </IconButton>
-          </Box>
-          {/* Button to open mobile menu */}
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{ display: { xs: "block", md: "none" }, ml: 1.5 }}
-            aria-controls={open ? "mobile-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-          >
-            <MenuRounded />
-          </IconButton>
-          {/* Mobile menu */}
-          <Menu
-            anchorEl={anchorEl}
-            id="mobile-menu"
-            open={open}
-            onClose={handleClose}
-            onClick={handleClose}
-            PaperProps={{
-              elevation: 0,
-              sx: {
-                overflow: "visible",
-                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                mt: 1.5,
-                "& .MuiAvatar-root": {
-                  width: 32,
-                  height: 32,
-                  ml: -0.5,
-                  mr: 1,
-                },
-                "&:before": {
-                  content: '""',
-                  display: "block",
-                  position: "absolute",
-                  top: 0,
-                  right: 14,
-                  width: 10,
-                  height: 10,
-                  bgcolor: "background.paper",
-                  transform: "translateY(-50%) rotate(45deg)",
-                  zIndex: 0,
-                },
-              },
-            }}
-            transformOrigin={{ horizontal: "right", vertical: "top" }}
-            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-          >
-            {/* Set goal button */}
-            <Link href="/goals/set" legacyBehavior>
-              <MenuItem>
-                <Button variant="contained">Set Goal</Button>
-              </MenuItem>
-            </Link>
-            {/* Account link */}
-            {isConnected && (
-              <Link href={`/accounts/${address}`} passHref legacyBehavior>
-                <MenuItem>Account</MenuItem>
-              </Link>
-            )}
-            {/* Explore goals link */}
-            <Link href="/goals" passHref legacyBehavior>
-              <MenuItem>Explore</MenuItem>
-            </Link>
-            <Divider />
-            {/* GitHub link */}
-            <MenuItem href={CONTACTS.github} target="_blank" component="a">
-              <ListItemIcon>
-                <GitHub fontSize="small" />
-              </ListItemIcon>
-              GitHub
-            </MenuItem>
-            {/* Twitter link */}
-            <MenuItem href={CONTACTS.twitter} target="_blank" component="a">
-              <ListItemIcon>
-                <Twitter fontSize="small" />
-              </ListItemIcon>
-              Twitter
-            </MenuItem>
-          </Menu>
-        </Toolbar>
-      </Container>
-    </AppBar>
+      <ConnectButton showBalance={false} accountStatus="address" />
+      <IconButton
+        onClick={handleClick}
+        size="small"
+        sx={{ display: { xs: "block", md: "none" }, ml: 1.5 }}
+        aria-controls={open ? "mobile-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+      >
+        <MenuRounded />
+      </IconButton>
+      <Menu
+        anchorEl={anchorEl}
+        id="mobile-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            mt: 1.5,
+            "& .MuiAvatar-root": {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            "&:before": {
+              content: '""',
+              display: "block",
+              position: "absolute",
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+      >
+        <Link href="/goals/set" legacyBehavior>
+          <MenuItem>
+            <Button variant="contained">Set Goal</Button>
+          </MenuItem>
+        </Link>
+        {isConnected && (
+          <Link href={`/accounts/${address}`} passHref legacyBehavior>
+            <MenuItem>Account</MenuItem>
+          </Link>
+        )}
+        <Link href="/goals" passHref legacyBehavior>
+          <MenuItem>Explore</MenuItem>
+        </Link>
+        <Divider />
+        <MenuItem component="a" target="_blank" href={CONTACTS.github}>
+          <ListItemIcon>
+            <GitHub fontSize="small" />
+          </ListItemIcon>
+          GitHub
+        </MenuItem>
+        <MenuItem component="a" target="_blank" href={CONTACTS.twitter}>
+          <ListItemIcon>
+            <Twitter fontSize="small" />
+          </ListItemIcon>
+          Twitter
+        </MenuItem>
+      </Menu>
+    </Box>
   );
 }
